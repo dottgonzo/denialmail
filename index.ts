5a14abd0758a610ed70f8634782005e106174c5c
Dario Caruso <darioyzf@gmail.com>
2016-05-06 21:27:28 +0200

    start

---
+++ index.ts
@@ -0,0 +1,58 @@
+
+import * as bluebird from "bluebird";
+
+import Nodemailer = require("nodemailer");
+
+interface Imail {
+
+    from: string;
+    to: string; // list of receivers 
+    subject: string; // Subject line 
+    text: string;
+    html: string;
+
+}
+
+
+export = class DenialMail {
+
+    from: string;
+    name: string;
+    to: string;
+    transporter: Nodemailer.Transporter;
+
+    constructor(from: string, password: string, to: string, fromname?: string) {
+
+        this.from = from;
+        this.to = from;
+
+        if (fromname) this.name = fromname;
+        this.transporter = Nodemailer.createTransport("smtps://" + from.split("@")[0] + "%40gmail.com:" + password + "@smtp.gmail.com");
+    }
+
+    send() {
+        return new Promise<boolean>(function(resolve, reject) {
+
+
+            let mailOptions: Imail = {
+                from: '"' + this.name + '" <' + this.from + '>', // sender address 
+                to: this.to, // list of receivers 
+                subject: "subject", // Subject line 
+                text: "text", // plaintext body 
+                html: "html" // html body 
+            };
+
+        
+            // send mail with defined transport object 
+            this.transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(true)
+                }
+                console.log('Message sent: ' + info.response);
+            });
+
+        })
+    }
+}
